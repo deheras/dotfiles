baca2f48b6942919c59ffc2ece6f1f565829ae3a
Nick de Heras <nickdeheras@gmail.com>
2022-07-01 18:37:38 -0400

    Add zsh aliases
    Aliases all consolidated in $ZSH_CUSTOM (per oh-my-zsh recommendations)

---
+++ dot_omz_custom/my-aliases.zsh
@@ -0,0 +1,35 @@
+# General
+alias fresh='exec "$SHELL"'
+alias q='exit'
+alias c='clear'
+alias h='history'
+alias cs='clear;ls'
+alias p='cat'
+alias pd='pwd'
+alias lsa='ls -a'
+alias lsl='ls -l'
+alias lsla='ls -l -a'
+alias pd='pwd'
+alias t='time'
+alias k='kill'
+alias home='cd ~'
+alias root='cd /'
+alias o=open
+alias ..='cd ..'
+alias ...='cd ..; cd ..'
+alias ....='cd ..; cd ..; cd ..'
+alias vimrc='vim ~/.vimrc'
+alias zshrc='vim ~/.zshrc'
+alias fresh='exec "$SHELL"'
+alias config='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
+
+# Mac stuff
+alias ql='qlmanage -p'
+
+# Django aliases
+alias dj='python manage.py'
+
+# Work stuff
+alias tunnel='~/Documents/projects/ap-advanced-analytics/tools/command-line-utils/ssh-tunnel/ssh_tunnel.sh'
+alias kdrive='open smb://crossix.local/data/'
+alias aarepo='~/Documents/projects/ap-advanced-analytics/'
